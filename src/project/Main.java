bb3a50fff02d1ef911cdd5ab83c12a50d137bb21
HJH <innodis.github@gmail.com>
2018-06-16 01:14:38 +0000

    로또 프로그램 작성

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,13 +1,23 @@
 package project;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Hello goorm!");
+    private Random random = new Random(System.nanoTime());
+    
+    public int[] generate() {
+        int[] result = new int[6];
+        
+        for(int i=0; i < 6; i++) {
+            result[i] = random.nextInt(45);
+        }
+        
+        return result;
     }
-
+    
+    public static void main(String[] args) {
+        int[] result = new Main().generate();
+        System.out.println(Arrays.toString(result));
+    }
 }
